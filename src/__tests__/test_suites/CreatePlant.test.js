345526cacd0fcf05a31e5f3f0f8bae38b6b5fc43
TaKeya McFadden <takeyamcfadden@gmail.com>
2024-02-08 15:37:32 -0500

    finally all test passing

--- src/__tests__/test_suites/CreatePlant.test.js
+++ src/__tests__/test_suites/CreatePlant.test.js
@@ -9,9 +9,9 @@ describe('2nd Deliverable', () => {
         const { getByPlaceholderText, findByText, getByText } = render(<App />)
 
         const firstPlant = {name: 'foo', image: 'foo_plant_image_url', price: '10'}
-    
+
         global.setFetchResponse(firstPlant)
-    
+
         fireEvent.change(getByPlaceholderText('Plant name'), { target: { value: firstPlant.name } });
         fireEvent.change(getByPlaceholderText('Image URL'), { target: { value: firstPlant.image } });
         fireEvent.change(getByPlaceholderText('Price'), { target: { value: firstPlant.price } });
@@ -24,19 +24,19 @@ describe('2nd Deliverable', () => {
             },
             body: JSON.stringify(firstPlant),
         })
-    
+
         const newPlant = await findByText('foo');
         expect(newPlant).toBeInTheDocument();
 
         const secondPlant = {name: 'bar', image: 'bar_plant_image_url', price: '5'}
-    
+
         global.setFetchResponse(secondPlant)
-    
+
         fireEvent.change(getByPlaceholderText('Plant name'), { target: { value: secondPlant.name } });
         fireEvent.change(getByPlaceholderText('Image URL'), { target: { value: secondPlant.image } });
         fireEvent.change(getByPlaceholderText('Price'), { target: { value: secondPlant.price } });
         fireEvent.click(getByText('Add Plant'))
-    
+
         expect(fetch).toHaveBeenCalledWith("http://localhost:6001/plants", {
             method: "POST",
             headers: {
